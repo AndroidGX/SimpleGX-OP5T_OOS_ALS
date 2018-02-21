a346bccc8d4f534f16f829cd96ce5298d1284e52
AndroidGX <subut3x@gmail.com>
2018-02-21 22:16:50 +0100

    v4.2

--- build_simplegx.sh
+++ build_simplegx.sh
@@ -12,7 +12,7 @@
 # Parameters to be configured manually
 #######################################
 
-SIMPLEGX_VERSION="OP5T-OOS-O_v4.1"
+SIMPLEGX_VERSION="OP5T-OOS-O_v4.2"
 
 TOOLCHAIN="/home/androidgx/projects/toolchains/google/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 ARCHITECTURE=arm64
